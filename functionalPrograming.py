23332bd5225df345666d635192df4184a7c25c13
MrRanjan12 <ranjankumar4447758@gmail.com>
2025-08-28 15:09:19 +0530

    added

--- functionalPrograming.py
+++ functionalPrograming.py
@@ -79,14 +79,166 @@
 # transformed into list using the list() funtion for easy viewing or further use.
 # 
 # # lsit of the name in variable 
-names = ["alice", "bob", "CHARLIE", "dEborah"]
-# Function to capitalize each name 
-def capitalize(name):
-    return name.capitalize()
-#using map() to apply the capitalizaton to each name 
-capitalize = map(capitalize, names)
-# converting map object to a list
-capitalize = list (capitalize)
-print(capitalize) 
+# names = ["alice", "bob", "CHARLIE", "dEborah"]
+# # Function to capitalize each name 
+# def capitalize(name):
+#     return name.capitalize()
+# #using map() to apply the capitalizaton to each name 
+# capitalize = map(capitalize, names)
+# # converting map object to a list
+# capitalize = list (capitalize)
+# print(capitalize) 
 #<------------------------------------->
+    # map funtion required  --->   map(<function>,<iterable)
+# exam_scores = [85, 62, 95, 40, 78]
+# def is_passing(score):
+#     return "Pass" if score >= 70 else "Fail"
+# status = list(map(is_passing,exam_scores))
+# print(status)
+
+#<----------------------------------------->
+# using lanmbda expression inside the map function
+# numbers = [1,2,3]
+# doubled = list(map(lambda x:x*2, numbers))
+# print(doubled)
+
+#<------------------------------------------->
+
+# The filter() function, just like the map() funtion, takes in a funtion 
+# and an iterable as arguments. The key purpose of filter() is to apply a 
+# condition specified in the provided funtion to each item in iterable and return 
+# and return only those for which the function evaluates to True
+
+# this code filters and returns product with names 4 characters long.
+
+# products = [" Table", "Sofa","Cushion","Bookshelf","vase"]
+# # filter products with name length equal to 4
+# filtered_prod = list(filter(lambda name: len(name) == 4, products))
+# print(filtered_prod)   
+
+#<--------------------------------------------->
+
+# The map() and filter() funtions can work with any iterable. 
+# In the example below, filter() is used to extract items from 
+# the products dictionary, where prices are under 90.
+
+# products = {"Table": 110, "Sofa": 120, "Chair": 45, "Lamp": 70}
+# # filtering products with prices less than 90 
+# filtered_products = dict(filter(lambda item: item[1] < 90, products.items()))
+# print(filtered_products)
+
+#<----------------------------------------------->
+
+# If the number of argument of our function is unknown and unpredictable,
+# you can always use an iterable as an argument.
+
+# def total(numbers):
+#     result = 0
+#     # iterating over the list 
+#     for i in numbers:
+#         result += i
+#     return result
+# nums = [1,2,3,4,5]
+# print(total(nums))
+
+#<--------------------------------------------------->
+
+# def display(*words):
+#     for item in words:
+#         print(item)
+
+# display("Paper", "Pen","pencil")
+
+#<----------------------------->
+# When defining a function with both regular arguments and *args, the 
+# regular arguments must come before *args in the function definiton.
+
+# for example
+# def show_items(category, *items):
+#     print("Categories: ", category)
+#     for item in items:
+#         print(item)
+# show_items("Electronics","laptop","Smartphone","tablets")    
+
+#<------------------------------->
+# Python also you to pass keyword argument using **Kwargs. In this 
+# case, **kwargs recieves arguments in the form of a dictionary, consisting of key:value pairs.
+
+#** kwargs is a dictionary 
+# def display_info(**kwargs):
+#     #kwargs.item() return the key:value pairs
+#     for key, value in kwargs.items():
+#         print(key, ":", value)
+# display_info(name = "Alice", age = 30, city = "New York")
+
+#<------------------------------->
+
+# Imagine you have a function that generates a message. your goal is to 
+# create another function that takes this original function as an
+# argument and converts the original message into uppercase, without altering 
+# the original function's code.
+# These functions are known as decorators. In the code below, the uppercase() function
+# acts as decorator, and the wrapper() funtion represents the modified (or decorated) 
+# version of the greet() function.
+
+# def greet():
+#     return "welcome!"
+# # takes a funtion as an argument 
+# def uppercase(func):
+#     #Wrapper function to keep the 
+#     #original function code unchanged 
+#     def wrapper():
+#         orig_message = func()
+#         modified_message = orig_message.upper()
+#         return modified_message
+#     return wrapper
+# gree_upper = uppercase(greet)
+# print(gree_upper())
+
+#<---------------------------->
+'''
+We can apply a decorator to a function using the @ sign. It 
+improves the code readability and provides a clean separation 
+between the function and its decoration.
+When a function with a decorator is called, it automatically include the 
+behaviour defiend in the decorator.
+'''
+# def uppercase(func):
+#     def wrapper():
+#         orig_message = func()
+#         modified_message = orig_message.upper()
+#         return modified_message
+#     return wrapper
+
+# @uppercase
+# def greet():
+#     return "Welcome!"
+
+# # using the decorated function 
+# print(greet())
+
+#<----------------------------------->
+
+"""
+Decorators are a powerful feature, offering a concise, readable,
+and efficient way to enhance the functionality of existing funtions.
+We can apply the same decorators to several different functions:
+"""
+
+# def stock_status_decorators(func):
+#     def wrapper(item):
+#         result = func(item)
+#         print(result, ": stock status for", item )
+#         return result
+#     return wrapper
+# @stock_status_decorators
+# def restock_item(item):
+#     return "Restocked"
+
+# @stock_status_decorators
+# def sell_item(item):
+#     return "Sold"
+
+# print(restock_item("Laptop"))
+# print(sell_item("Smartphone"))
 
--- oops.py
+++ oops.py
@@ -12,59 +12,152 @@
 
 # now connecting to the code with linkedList
 
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
+# class Node:
+#     def __init__(self, data):
+#         self.data = data
+#         self.next = None
 
-class LinkedList:
-    def __init__(self):
-        self.head = None       
+# class LinkedList:
+#     def __init__(self):
+#         self.head = None       
 
-    def insert_end(self, data):
-        new_node = Node(data)
-        if not self.head:
-            self.head = new_node
-            return
-        temp = self.head
-        while temp.next:
-            temp = temp.next
-        temp.next = new_node
+#     def insert_end(self, data):
+#         new_node = Node(data)
+#         if not self.head:
+#             self.head = new_node
+#             return
+#         temp = self.head
+#         while temp.next:
+#             temp = temp.next
+#         temp.next = new_node
 
-    def insert_begin(self, data):
-        new_node = Node(data)
-        new_node.next = self.head
-        self.head = new_node
+#     def insert_begin(self, data):
+#         new_node = Node(data)
+#         new_node.next = self.head
+#         self.head = new_node
     
-    def delete(self, key):
-        temp = self.head
-        if temp and temp.data == key:
-            self.head = temp.next
-            return
-        prev = None
-        while temp and temp.data != key:
-            prev = temp
-            temp = temp.next
-        if temp:
-            prev.next = temp.next
+#     def delete(self, key):
+#         temp = self.head
+#         if temp and temp.data == key:
+#             self.head = temp.next
+#             return
+#         prev = None
+#         while temp and temp.data != key:
+#             prev = temp
+#             temp = temp.next
+#         if temp:
+#             prev.next = temp.next
 
     
-    def display(self):
-        temp = self.head
-        while temp:
-            print(temp.data, end = " -> ")
-            temp = temp.next
-        print("None")
+#     def display(self):
+#         temp = self.head
+#         while temp:
+#             print(temp.data, end = " -> ")
+#             temp = temp.next
+#         print("None")
 
-#Creating and inserting 
+# #Creating and inserting 
 
-my_list = LinkedList()  # empty list
-my_list.insert_end("R") 
-my_list.insert_end("A")
-my_list.insert_end("N")
-my_list.insert_begin("J")
-my_list.delete("A")
+# my_list = LinkedList()  # empty list
+# my_list.insert_end("R") 
+# my_list.insert_end("A")
+# my_list.insert_end("N")
+# my_list.insert_begin("J")
+# my_list.delete("A")
 
-# displaying the output 
+# # displaying the output 
 
-my_list.display()
+# my_list.display()
+
+#<------------------>
+
+# class Car:
+#     # initilize attributes 
+#     def __init__(self, brand, color):
+#         # assign values to attributes
+#         self.brand = brand
+#         self.color = color
+# # Create an object of the Car Class 
+# my_car = Car("Audi", "yellow")
+# print(my_car.color)
+        
+        #<---------->
+# # Inheritance
+# class Animal:
+#     def __init__(self, name):
+#         self.name = name
+
+#     def move(self):
+#         print("Moving")
+# #Inherits from Animal class 
+# class Dog(Animal):
+#     #Specific behaviour 
+#     def bark(self):
+#         print("woof!")
+# #Creating and instance 
+# my_dog = Dog("Bob")
+
+# # inherited attribute and behavior 
+# print(my_dog.name)
+# my_dog.move()
+# # Specific behavior
+# my_dog.bark()
+
+#<-------------------------->
+
+#If a child class needs both inherited and new attributes,
+#  define its own __init__ method, call super().__init__() to reuse the parent’s
+#  attributes, and then add the extra ones normally.
+
+# parent class 
+# class Animal:
+#     def __init__(self, name):
+#         self.name = name
+    
+#     def move(self):
+#         print("Moving")
+# # child class 
+# class Dog(Animal):
+#     def __init__(self, name, breed, age):
+#         # initialize attributes of the supperclass
+#         super().__init__(name)
+#         #Additional attributes specific to DOg
+#         self.breed = breed
+#         self.age = age
+    
+#     def bark(self):
+#         print("Woof!")
+
+# my_dog = Dog("Jax", "Bulldog", 5)
+# #Inherited attribute
+# print(my_dog.name)
+
+# # Additional attributes
+# print(my_dog.breed)
+# print(my_dog.age)
+  #<=====================>
+# Class methods are called on the class itself, not on 
+# indivisual instances. This allows their use without needing to create 
+# a class as a whole, rather than actions limited to single object.
+# Here's an example:
+
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+# regular method 
+    def describe_book(self):
+        print(self.title, "by", self.author)
+
+# class method 
+    @classmethod
+    def books_in_series(cls, series_name, number_of_books):
+        print("There are ", number_of_books, "books in the", series_name, "series")
+
+#Creating and instance of book
+my_book = Book("Harry Potter and the Sorcerer's", "J.K. Rowling")
+
+#Using the instance method to describe the book
+my_book.describe_book()
+#Using the class method to display information about the series 
+Book.books_in_series("Harry Potter", 7)
